c490c8e9780ed73d28f216a27566742e61180b17
Megan Horsfield <meganthadev@gmail.com>
2020-08-30 04:55:28 +0000

    Automatically backed up by Learn

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -6,8 +6,10 @@ export default function shoppingListItemReducer(
 ) {
     console.log(action);
 	switch (action.type) {
-		case 'INCREASE_COUNT':console.log('Current state.items length %s', state.items.length);
+		case 'INCREASE_COUNT':
+		  console.log('Current state.items length %s', state.items.length);
       console.log('Updating state.items length to %s', state.items.length + 1);
+      
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
